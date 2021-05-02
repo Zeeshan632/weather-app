0efe005df1b29e1e4257e70a24ed13f0030e2aa4
Zeeshan632 <zeeshanadil632@gmail.com>
2021-05-02 10:33:19 +0500

    just reading the code again

--- scripts/app.js
+++ scripts/app.js
@@ -1,84 +1,75 @@
-const form = document.querySelector('form');
-const card = document.querySelector('.card');
-const details = document.querySelector('.details');
-const time = document.querySelector('img.time')
-const icon = document.querySelector('.icon > img');
+const form = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon > img");
 
 //CALLING THE CONSTRUCTOR CLASS FOR INFORMATION FROM FORECAST.JS
 const weather = new WeatherInformation();
 
-
 //local storage function
 const storingData = (location) => {
-    localStorage.setItem('weatherData', location);
-}
-
-
+	localStorage.setItem("weatherData", location);
+};
 
 // FUNTION THAT IS UPDATING THE UI
 const updateUI = (data) => {
-    const {cityDet, weatherDet} = data; 
+	const { cityDet, weatherDet } = data;
 
-    if(weatherDet.IsDayTime){
-        time.setAttribute('src', 'images/day-time.jpg');
-        storingData(cityDet.EnglishName);
+	if (weatherDet.IsDayTime) {
+		time.setAttribute("src", "images/day-time.jpg");
+		storingData(cityDet.EnglishName);
 
-        details.innerHTML = 
-        `<h5>${cityDet.EnglishName}</h5>
+		details.innerHTML = `<h5>${cityDet.EnglishName}</h5>
         <div>Day Time, ${weatherDet.WeatherText}</div>
         <p>
             <span>${weatherDet.Temperature.Metric.Value}</span>
             <span>&deg;C</span>
         </p>`;
-    }
-    else {
-        time.setAttribute('src', 'images/night-time.jpg');
-        storingData(cityDet.EnglishName);
+	} else {
+		time.setAttribute("src", "images/night-time.jpg");
+		storingData(cityDet.EnglishName);
 
-        details.innerHTML = 
-        `<h5>${cityDet.EnglishName}</h5>
+		details.innerHTML = `<h5>${cityDet.EnglishName}</h5>
         <div>Night Time, ${weatherDet.WeatherText}</div>
         <p>
             <span>${weatherDet.Temperature.Metric.Value}</span>
             <span>&deg;C</span>
-        </p>`
-    }
-    icon.setAttribute('src', `icons/${weatherDet.WeatherIcon}.svg`)
+        </p>`;
+	}
+	icon.setAttribute("src", `icons/${weatherDet.WeatherIcon}.svg`);
 
-    //checking if card has a d-none class
-    if(card.classList.contains('d-none')){
-        card.classList.remove('d-none');
-    }
-}
-
-
+	//checking if card has a d-none class
+	if (card.classList.contains("d-none")) {
+		card.classList.remove("d-none");
+	}
+};
 
 //UPDATING OR ADDING THE WEATHER INFO TO CARD WHEN THE INPUT IS SUBMITTED
-form.addEventListener('submit', (e) => {
-    //prevent default location
-    e.preventDefault();
-    
-    //get city value
-    const city = form.city.value.trim();
-    form.reset();
+form.addEventListener("submit", (e) => {
+	//prevent default location
+	e.preventDefault();
 
-    //update the UI with new city
-    weather.weatherInfo(city)
-        .then(data => {
-            updateUI(data);
-        })
-        .catch(err => {
-            details.innerHTML = err;
-            card.classList.remove('d-none');
-        });
-})
+	//get city value
+	const city = form.city.value.trim();
+	form.reset();
+
+	//update the UI with new city
+	weather
+		.weatherInfo(city)
+		.then((data) => {
+			updateUI(data);
+		})
+		.catch((err) => {
+			details.innerHTML = err;
+			card.classList.remove("d-none");
+		});
+});
 
 //ADDING THE WEATHER INFORMATION FROM THE LOCATION THAT HAS ALREADY STORED INTO THE LOCAL STORAGE
-document.addEventListener('DOMContentLoaded', () => {
-    const city = localStorage.getItem('weatherData');
-    weather.weatherInfo(city).then((data) => {
-        updateUI(data);
-    })
-})
-
-
+document.addEventListener("DOMContentLoaded", () => {
+	const city = localStorage.getItem("weatherData");
+	weather.weatherInfo(city).then((data) => {
+		updateUI(data);
+	});
+});
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,25 +1,27 @@
-
 class WeatherInformation {
-    constructor(){
-        this.key = 'WD0595mOPbpVa11X0w3jxGrpoeAA0FgM';
-        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
-        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search'; 
-    }
-    async getWeather(id){
-        const response = await fetch(this.weatherURI + `${id}?apikey=${this.key}`);
-        const data = await response.json();
-        return data[0]; 
-    }
-    async getCity(city){
-        const response = await fetch(this.cityURI + `?apikey=${this.key}&q=${city}`);
-        const data = await response.json();
-        return data[0]
-    }
-    async weatherInfo(city){
-        const cityDet = await this.getCity(city);
-        const weatherDet = await this.getWeather(cityDet.Key);
-        return { cityDet, weatherDet}
-    }
+	constructor() {
+		this.key = "WD0595mOPbpVa11X0w3jxGrpoeAA0FgM";
+		this.weatherURI =
+			"http://dataservice.accuweather.com/currentconditions/v1/";
+		this.cityURI =
+			"http://dataservice.accuweather.com/locations/v1/cities/search";
+	}
+
+	async getWeather(id) {
+		const response = await fetch(this.weatherURI + `${id}?apikey=${this.key}`);
+		const data = await response.json();
+		return data[0];
+	}
+	async getCity(city) {
+		const response = await fetch(
+			this.cityURI + `?apikey=${this.key}&q=${city}`
+		);
+		const data = await response.json();
+		return data[0];
+	}
+	async weatherInfo(city) {
+		const cityDet = await this.getCity(city);
+		const weatherDet = await this.getWeather(cityDet.Key);
+		return { cityDet, weatherDet };
+	}
 }
-
-
